01c9dc09becee215645c83149ab8073b92a5e2f9
phantomkid <adhiman1999@gmail.com>
2021-06-22 10:13:25 +0530

    intant message display

--- client/src/components/Chat/ChatBox.jsx
+++ client/src/components/Chat/ChatBox.jsx
@@ -100,12 +100,12 @@ const ChatBox = (props) => {
       text: newMessage,
     });
     setMessages([...messages, message]);
-    setMessages("");
     try {
       const res = await axios.post(
         "https://morning-scrubland-01222.herokuapp.com/message",
         message
       );
+      setMessages("");
       console.log(res);
 
       //to clear the input after sending message
